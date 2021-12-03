312a650a2b0f726ef233fd9d764c17af91cb4feb
Kirillova Olesya <olesyak-85@mail.ru>
2021-12-03 22:00:50 +0300

    create mainPage

--- src/test/java/ru/netology/page/MainPage.java
+++ src/test/java/ru/netology/page/MainPage.java
@@ -1,4 +1,28 @@
 package ru.netology.page;
 
-public class MainPage  {
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class MainPage{
+    private SelenideElement heading = $("h2.heading");
+    private SelenideElement buttonBuyCard = $("[class=button__content]");
+    private SelenideElement buttonBuyCredit = $$("[class=button__content]").get(1);
+
+    public MainPage() {
+        heading.shouldBe(visible);
+    }
+
+    public BuyByCard getPageByCard(){
+        buttonBuyCard.click();
+        return new BuyByCard();
+    }
+
+    public BuyOnCredit getPageOnCredit(){
+        buttonBuyCredit.click();
+        return new BuyOnCredit();
+    }
+
 }
