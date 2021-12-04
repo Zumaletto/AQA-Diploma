51e04847d08d09d947519f87a6f3e30b4f082ddd
Kirillova Olesya <olesyak-85@mail.ru>
2021-12-04 17:49:13 +0300

    modified application.properties

--- src/test/java/ru/netology/page/BuyByCard.java
+++ src/test/java/ru/netology/page/BuyByCard.java
@@ -1,4 +1,42 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class BuyByCard {
+    private SelenideElement heading = $$(".heading").findBy(text("Оплата по карте"));
+    private SelenideElement cardNumberCard = $$(".input__control").get(0);
+    private SelenideElement monthCard = $$(".input__control").get(1);
+    private SelenideElement yearCard = $$(".input__control").get(2);
+    private SelenideElement ownerCard = $$(".input__control").get(3);
+    private SelenideElement cvcCard = $$(".input__control").get(4);
+    private SelenideElement buttonCard = $$("button").findBy(text("Продолжить"));
+
+    public BuyByCard(){
+        heading.shouldBe(visible);
+    }
+
+    public void validPaymentGate(){
+        cardNumberCard.setValue(DataHelper.getApprovedCardNumber());
+        monthCard.setValue(DataHelper.getValidMonth());
+        yearCard.setValue(DataHelper.getValidYear());
+        ownerCard.setValue(DataHelper.getValidOwner());
+        cvcCard.setValue(DataHelper.getValidCvc());
+        buttonCard.click();
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/test/java/ru/netology/page/BuyOnCredit.java
+++ src/test/java/ru/netology/page/BuyOnCredit.java
@@ -1,4 +1,18 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class BuyOnCredit {
+
+    private SelenideElement heading = $$(".heading").findBy(text("Кредит по данным карты"));
+    private SelenideElement cardNumberCredit = $$(".input__control").get(0);
+    private SelenideElement monthCredit = $$(".input__control").get(1);
+    private SelenideElement yearCredit = $$(".input__control").get(2);
+    private SelenideElement ownerCredit = $$(".input__control").get(3);
+    private SelenideElement cvcCredit = $$(".input__control").get(4);
+    private SelenideElement buttonCredit = $$("button").findBy(text("Продолжить"));
+
 }
